4f3257e12af7085453d857e0db828f4451225e24
samuel kisenge <ksksamuelkis@gmail.com>
2026-02-03 01:19:12 +0100

    changement de port serveur

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [ 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://localhost:3000' ],
     credentials: true
 }));
 app.use(morgan('dev'));
